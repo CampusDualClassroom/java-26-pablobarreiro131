d7a95bb6bf2346a117f1d137ba0070551da8dc87
Pablo <pablobarreiro131@gmail.com>
2025-05-26 10:32:05 +0200

    Ejercicio 26, completado y testeado.

--- src/main/java/com/campusdual/classroom/Contact.java
+++ src/main/java/com/campusdual/classroom/Contact.java
@@ -1,5 +1,95 @@
 package com.campusdual.classroom;
+import java.text.Normalizer;
+import java.util.Locale;
 
-public class Contact{
+public class Contact implements ICallActions{
 
+    private String name;
+    private String surnames;
+    private String phone;
+    private String code;
+    Phonebook phonebook;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getSurnames() {
+        return surnames;
+    }
+
+    public void setSurnames(String surnames) {
+        this.surnames = surnames;
+    }
+
+    public void generateCode(){
+        String normalizedName = Normalizer.normalize(this.name, Normalizer.Form.NFD).replaceAll("\\p{InCombiningDiacriticalMarks}+", "").toLowerCase(Locale.ROOT);
+        String normalizedSurnames = Normalizer.normalize(this.surnames, Normalizer.Form.NFD).replaceAll("\\p{InCombiningDiacriticalMarks}+", "").toLowerCase(Locale.ROOT);
+        String [] parts = normalizedSurnames.split(" ");
+        StringBuilder sb = new StringBuilder();
+        sb.append(normalizedName.charAt(0));
+        if(parts.length == 1){
+            sb.append(parts[0]);
+        }
+        else {
+                sb.append(parts[0].charAt(0));
+                for (int i = 1; i < parts.length; i++) {
+                    sb.append(parts[i]);
+            }
+        }
+        this.code = sb.toString();
+    }
+
+    public Contact(String name, String surnames, String phone){
+        this.name= name;
+        this.surnames= surnames;
+        this.phone= phone;
+        generateCode();
+    }
+
+    public String getCode() {
+        return code;
+    }
+    public void setPhonebook(Phonebook phonebook) {
+        this.phonebook = phonebook;
+    }
+
+
+    public void callMyNumber() {
+        System.out.println("Te estas llamando a ti mismo... " + this.getName() + " " + this.getSurnames() + " Numero: "+ this.getPhone());
+    }
+
+    public void callOtherNumber(String phone) {
+        String calledName = "Contacto desconocido";
+
+        if (phonebook != null) {
+            Contact calledContact = phonebook.getContactByPhone(phone);
+            if (calledContact != null) {
+                calledName = calledContact.getName() + " " + calledContact.getSurnames();
+            }
+        }
+
+        System.out.println(name + " " + surnames + " está llamando a " + calledName + " (Número: " + phone + ")");
+    }
+
+
+
+    public void showContactDetails() {
+        System.out.println("Nombre: " + this.name);
+        System.out.println("Apellidos: " + this.surnames);
+        System.out.println("Telefono: " + this.phone);
+        System.out.println("Codigo: " + this.code);
+    }
 }
--- src/main/java/com/campusdual/classroom/Phonebook.java
+++ src/main/java/com/campusdual/classroom/Phonebook.java
@@ -1,5 +1,120 @@
 package com.campusdual.classroom;
 
+import java.sql.SQLOutput;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Phonebook {
 
+    private HashMap<String, Contact> data = new HashMap<>();
+    Scanner teclado = new Scanner(System.in);
+    Contact tuMismo;
+
+    public Phonebook(){
+        this.tuMismo = new Contact("Pepe", "Perez Gomez", "666555444");
+        this.tuMismo.setPhonebook(this);
+    }
+
+    public void addContact(Contact c) {
+        data.put(c.getCode(),c);
+        c.setPhonebook(this);
+    }
+    public void showPhonebook() {
+        for (Contact c: data.values()){
+            System.out.println("Nombre: " + c.getName());
+            System.out.println("Apellidos: " + c.getSurnames());
+            System.out.println("Telefono: " + c.getPhone());
+            System.out.println("Codigo: " + c.getCode());
+            System.out.println("------------------------------------------");
+        }
+    }
+
+    public void deleteContact(String code){
+        data.remove(code);
+    }
+
+    public void menu(){
+        boolean fin = true;
+        do{
+            System.out.println("||| Menu principal |||");
+            System.out.println("1. Añadir contacto");
+            System.out.println("2. Mostrar contactos");
+            System.out.println("3. Seleccionar contacto: ");
+            System.out.println("4. Eliminar contacto");
+            System.out.println("5. Mostrar mis datos");
+            System.out.println("0. Salir");
+            System.out.print("Opción: ");
+            int eleccion = teclado.nextInt();
+            teclado.nextLine();
+            switch(eleccion){
+                case 1: {
+                    System.out.println("Añadiendo contacto...");
+                    System.out.print("Nombre: ");
+                    String name = teclado.nextLine();
+                    System.out.print("Apellidos: ");
+                    String surnames = teclado.nextLine();
+                    System.out.print("Telefono: ");
+                    String phone = teclado.nextLine();
+                    Contact c = new Contact(name, surnames, phone);
+                    addContact(c);
+                    System.out.println("Contacto añadido con código: " + c.getCode()); break;
+                }
+                case 2: showPhonebook(); break;
+                case 3:
+                    System.out.print("Introduce el c del contacto: ");
+                    String codigo = teclado.nextLine();
+                    menuContacto(data.get(codigo)); break;
+                case 4: {
+                    System.out.print("Introduce el codigo del contacto a eliminar:  ");
+                    String code = teclado.nextLine();
+                    deleteContact(code);
+                    System.out.println("Contacto eliminado."); break;
+                }
+                case 5: tuMismo.showContactDetails(); break;
+                case 0: fin = false; break;
+                default:
+                    System.out.println("La opción elegida no es valida."); break;
+            }
+        }
+        while(fin);
+    }
+
+    public void menuContacto(Contact c){
+        boolean salida = true;
+        int opc;
+
+        do{
+            System.out.println("1. Llamarte a ti mismo");
+            System.out.println("2. Llamar al contacto introducido");
+            System.out.println("3. Informacion del contacto.");
+            System.out.println("0. Salir");
+            System.out.print("Opción: ");
+            opc = teclado.nextInt();
+            teclado.nextLine();
+            switch (opc){
+                case 1: tuMismo.callMyNumber(); break;
+                case 2: {
+                    tuMismo.callOtherNumber(c.getPhone()); break;
+                }
+                case 3: c.showContactDetails();
+                case 0: salida = false; break;
+            }
+
+        }
+        while(salida);
+
+    }
+
+    public Map<String, Contact> getData() {
+        return this.data;
+    }
+    public Contact getContactByPhone(String phone) {
+        for (Contact contact : data.values()) {
+            if (contact.getPhone().equals(phone)) {
+                return contact;
+            }
+        }
+        return null;
+    }
 }
